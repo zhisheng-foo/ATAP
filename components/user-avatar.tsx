f673c65a6fde0adfbaa970d97a0f63c70337712e
Foo Zhi Sheng <105271950+zhisheng-foo@users.noreply.github.com>
2024-06-21 03:19:11 +0800

    Member modal

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -4,12 +4,12 @@ import { cn } from '@/lib/utils';
 
 interface UserAvatarProps {
     src?: string;
-    classname?: string;
+    className?: string;
 }
 
-const UserAvatar: FC<UserAvatarProps> = ({ classname, src }) => {
+const UserAvatar: FC<UserAvatarProps> = ({ className, src }) => {
     return (
-        <Avatar className={cn('h-7 w-7 md:h-10 md:w-10', classname)}>
+        <Avatar className={cn('h-7 w-7 md:h-10 md:w-10', className)}>
             <AvatarImage src={src} />
         </Avatar>
     );
