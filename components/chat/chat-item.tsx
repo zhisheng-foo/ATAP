6fbd452234a2b02d8590b87f2ae1ed1d55d689c8
Foo Zhi Sheng <105271950+zhisheng-foo@users.noreply.github.com>
2024-07-16 17:52:38 +0800

    Chat convo in progress

---
+++ components/chat/chat-item.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { Member, Profile , MemberRole } from "@prisma/client";
+import  UserAvatar  from "@/components/user-avatar";
+import Image from 'next/image';
+import { ActionTooltip } from "../action-tooltip";
+
+import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+
+interface ChatItemProps {
+    id: string;
+    content: string;
+    member : Member & {
+        profile: Profile;
+    };
+    timestamp: string;
+    fileUrl: string | null;
+    deleted: boolean;
+    currentMember: Member;
+    isUpdated: boolean;
+    socketUrl: string;
+    socketQuery: Record<string, string>;
+};
+
+const roleIconMap={
+    "GUEST": null,
+    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+    "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
+}
+
+export const ChatItem = ({
+    id,
+    content,
+    member,
+    timestamp,
+    fileUrl,
+    deleted,
+    currentMember,
+    isUpdated,
+    socketUrl,
+    socketQuery
+
+}: ChatItemProps) => {
+
+    const fileType = fileUrl?.split(".").pop()
+
+    const isAdmin = currentMember.role === MemberRole.ADMIN;
+    const isModerator = currentMember.role === MemberRole.MODERATOR;
+    const isOwner = currentMember.role === member.id;
+    const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+    const canEditMessage = !deleted && isOwner && !fileUrl;
+    const isPDF = fileType === "pdf" && fileUrl;
+    const isImage = !isPDF && fileUrl;
+
+
+    return (
+        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+            <div className="group flex gap-x-2 items-start w-full">
+                <div className="cursor-pointer hover:drop-shadow-md transition">
+                    <UserAvatar src={member.profile.imageUrl} />
+                </div>
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center">
+                        <p className="font-semibold text-sm hover:underline cursor-pointer">
+                            {member.profile.name}
+                        </p>
+                        <ActionTooltip label={member.role}>
+                            {roleIconMap[member.role]}
+                        </ActionTooltip>
+                    </div>
+                    <span className="ttext-xs text-zinc-500">
+                        {timestamp}
+                    </span>
+                    {isImage && (
+                        <a 
+                            href={fileUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative aspect-square rounded-md mt-2 
+                            overflow-hidden border flex items-center bg-secondary h-48 w-48">
+                            <Image 
+                                src={fileUrl}
+                                alt={content}
+                                fill
+                                className="object-cover"
+                            />
+                        </a>
+                    )}
+                    {isPDF && (
+                        <div className="relative flex items-center =-p-2 mt-2 rounded-md bg-background/10">
+                            <FileIcon className="h-10 w-10 fill-indigo-200 strike-indigo-400" />
+                            <a 
+                                href={fileUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="ml-2 text-sm text-indigo-500 
+                                dark:text-indigo-400 hover:underline"
+            
+                            >
+                                PDF File
+                            </a>         
+                        </div>
+                    )}
+                </div>
+            </div>  
+        </div>
+    )
+}
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
+import { Fragment } from "react";
 import { ChatWelcome } from "./chat-welcome";
+import { format } from "date-fns";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
+import { ChatItem } from "./chat-item";
 
+const DATE_FORMAT = "d MMM yyy, HH:mm";
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+
+ }
 interface ChatMessagesProps {
     name: string;
     member: Member;
@@ -26,6 +39,46 @@ export const ChatMessages = ({
     paramValue,
     type,
 }: ChatMessagesProps) => {
+
+    const queryKey= `chat:${chatId}`;
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status
+
+    } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue,
+    });
+
+    if (status === "pending") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                <p className="mt-2 text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading messages...
+                </p>
+            </div>
+        )
+    }
+    
+
+    if (status === "error") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                
+                <p className="mt-2 text-xs text-zinc-500 dark:text-zinc-400">
+                    Something went wrong!
+                </p>
+            </div>
+        )
+    }
+    
     return (
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1" />
@@ -33,6 +86,29 @@ export const ChatMessages = ({
                type={type}
                name={name}
             /> 
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group, i) => (
+                    <Fragment key={i}>
+                        {group.items.map((message: MessageWithMemberWithProfile) => (
+                            <ChatItem
+                                key={message.id}
+                                id={message.id}
+                                currentMember={member}
+                                member={message.member}
+                                content={message.content}
+                                fileUrl={message.fileUrl}
+                                deleted={message.deleted}
+                                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                isUpdated={message.updatedAt !== message.createdAt}
+                                socketUrl={socketUrl}
+                                socketQuery={socketQuery}
+                            />
+
+                        ))}
+                    </Fragment>
+
+                ))}    
+            </div>
         </div>
     )
 }
